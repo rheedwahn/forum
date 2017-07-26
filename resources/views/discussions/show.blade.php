e14a595d4a2ce49b6c44e696e70e55b3988ec49c
rheedwahn <rabusari@student.lautech.edu.ng>
2017-07-26 12:12:08 +0100

    new change

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -82,7 +82,7 @@
                         <div class="panel-heading">
                             <img src="{{ asset($r->user->avater) }}" width="40px" height="40px" alt="" /> &nbsp;&nbsp;
                             <span>{{ $r->user->name }}, <b><i>{{ $r->created_at->diffForHumans() }}</i></b></span>
-
+                            @if(!empty($best_answer))
                             @if(!$best_answer)
                                 @if(Auth::user()->id === $r->user->id)
                                     <a href="{{ route('reply.edit', ['id' => $r->id]) }}" class="btn btn-primary btn-xs pull-right" style="margin-left: 9px;"> Edit Reply</a>
@@ -90,6 +90,7 @@
                                 @if(Auth::user()->id === $d->user_id)
                                     <a href="{{ route('reply.bestanswer', ['id' => $r->id]) }}" class="btn btn-info btn-xs pull-right" > Mark as best answer</a>  
                                 @endif
+                            @endif
                             @endif
                         </div>
 
