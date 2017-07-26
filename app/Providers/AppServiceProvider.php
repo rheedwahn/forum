4b6d188fe8fbde1673f3b9aa69f4846d51ec77c8
rheedwahn <rabusari@student.lautech.edu.ng>
2017-07-26 10:56:34 +0100

    channel changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        // View::share('channels', Channel::all());
+        View::share('channels', Channel::all());
     }
 
     /**
