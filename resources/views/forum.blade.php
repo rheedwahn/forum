f873809e8b6da94cebe56e257b50ce671fda1781
rheedwahn <rabusari@student.lautech.edu.ng>
2017-07-26 19:29:15 +0100

    updated

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -22,7 +22,7 @@
                     <b>{{ $d->title }}</b>
                 </h4>
                 <p class="text-center">
-                    {{ str_limit($d->content, 100) }}
+                    {!! Markdown::convertToHtml(str_limit($d->content, 100)) !!}
                 </p>
             </div>
 
